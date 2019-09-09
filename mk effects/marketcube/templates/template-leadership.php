33e854e46b4ac7f3f049ec646bb3e42ef19facbc
Anees <anees.n@acodez.co.in>
2019-09-09 18:19:03 +0530

    mk effets added

---
+++ mk effects/marketcube/templates/template-leadership.php
@@ -0,0 +1,31 @@
+<?php  
+/* 
+  Template Name: leadership page
+ */
+
+get_header();
+
+add_action('wp_footer', 'san_scripts', 21);
+
+function san_scripts() {
+?>
+
+
+
+<?php } ?>
+<!-- container section -->
+
+   
+    
+  
+
+<div class="container-leadership">
+
+
+<h2>sadasdd</h2>
+<h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum, eos neque tempore molestiae enim totam maiores aut corrupti ducimus nesciunt, necessitatibus dolorum explicabo doloribus fugiat voluptatem commodi! Obcaecati, cum voluptates!</h3>
+</div>
+
+
+
+<?php get_footer(); ?>
